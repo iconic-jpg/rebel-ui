c9d9a3304ff0f49f51c8bd44e9efd7286d443eb9
icontroller-jpg <walternyika07@gmail.com>
2026-03-05 01:24:59 +0530

    lets win

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import RebelDashboard from "./components/RebelDashboard/RebelDashboard";
-import Login from "./components/Auth/Login";
-import Signup from "./components/Auth/Signup";
-
+import RebelDashboard from "./components/RebelDashboard/RebelDashboard.js";
+import Login from "./components/Auth/Login.js";
+import Signup from "./components/Auth/Signup.js";
 export default function App() {
   return (
     <Router>
